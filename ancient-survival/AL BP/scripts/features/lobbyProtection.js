11448d7a25c4af22311f9ada182da0ef63aa76cd
Darkness <darkness.9373@gmail.com>
2026-02-15 19:56:37 +0700

    up lobby protection

--- ancient-survival/AL BP/scripts/features/lobbyProtection.js
+++ ancient-survival/AL BP/scripts/features/lobbyProtection.js
@@ -1,4 +1,5 @@
-import { system, world } from '@minecraft/server';
+import { GameMode, Player, system, world } from '@minecraft/server';
+import { text } from '../config/text';
 
 const LOBBY1 = { x: -100, y: 190, z: -150 };
 const LOBBY2 = { x: 140, y: 320, z: 35 };
@@ -14,9 +15,25 @@ function isInLobby(pos) {
 system.runInterval(() => {
   world.getPlayers().forEach(player => {
     const pos = player.location
-    
+    if (player.hasTag('admin')) return
     if (isInLobby(pos)) {
-      
+        if (player.getGameMode() === GameMode.Survival) {
+          player.setGameMode(GameMode.Adventure)
+          player.sendMessage(text('Kamu berada di area Lobby, GameMode kamu akan diubah ke mode Adventure').System.deff)
+        }
+    } else {
+      if (player.getGameMode() === GameMode.Adventure) {
+        player.sendMessage(text('Kamu keluar dari area Lobby, Gamemode kamu akan kembali ke mode Survival').System.deff)
+      }
     }
   })
-}, 20)
+}, 2)
+
+world.beforeEvents.entityHurt.subscribe(data => {
+  const player = data.hurtEntity
+  if (!(player instanceof Player)) return
+  const pos = player.location
+  if (isInLobby(pos)) {
+    data.cancel = true;
+  }
+})
